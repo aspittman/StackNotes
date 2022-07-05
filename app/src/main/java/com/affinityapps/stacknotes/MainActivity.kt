35b76a00fd54b9f1c9d526c86beaec59350a1793
aspittman <aspittman@live.com>
2022-07-05 17:53:53 -0600

    Basic RecyclerView implemented

--- app/src/main/java/com/affinityapps/stacknotes/MainActivity.kt
+++ app/src/main/java/com/affinityapps/stacknotes/MainActivity.kt
@@ -2,10 +2,32 @@ package com.affinityapps.stacknotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val randomList = ArrayList<String>()
+        randomList.add("aaaaa")
+        randomList.add("bbbbb")
+        randomList.add("ccccc")
+        randomList.add("ddddd")
+        randomList.add("eeeee")
+
+       val recyclerView:RecyclerView = findViewById(R.id.main_grid)
+        GridLayoutManager(
+            this, // context
+            2, // span count
+            RecyclerView.VERTICAL, // orientation
+            false // reverse layout
+        ).apply {
+            // specify the layout manager for recycler view
+            recyclerView.layoutManager = this
+        }
+        recyclerView.adapter = NoteAdapter(randomList)
+
     }
 }
